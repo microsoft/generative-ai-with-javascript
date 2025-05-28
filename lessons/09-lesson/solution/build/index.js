209f9c0eac8664002f0c6bbc611b1a5cd3f232fe
Chris <chnoring@microsoft.com>
2025-05-28 22:42:35 +0100

    adding sse solution draft

--- lessons/09-lesson/solution/build/index.js
+++ lessons/09-lesson/solution/build/index.js
@@ -1,52 +1,42 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
-
 import express from "express";
-
 const server = new McpServer({
     name: "example-server",
     version: "1.0.0"
 });
-
 const app = express();
-
-const transports: {[sessionId: string]: SSEServerTransport} = {};
-
-app.get("/sse", async (_: Request, res) => {
-        const transport = new SSEServerTransport('/messages', res);
-        transports[transport.sessionId] = transport;
-        res.on("close", () => {
-            delete transports[transport.sessionId];
-        });
-        await server.connect(transport);
+const transports = {};
+app.get("/sse", async (_, res) => {
+    const transport = new SSEServerTransport('/messages', res);
+    transports[transport.sessionId] = transport;
+    res.on("close", () => {
+        delete transports[transport.sessionId];
     });
-    
+    await server.connect(transport);
+});
 app.post("/messages", async (req, res) => {
-    const sessionId = req.query.sessionId as string;
+    const sessionId = req.query.sessionId;
     const transport = transports[sessionId];
     if (transport) {
         await transport.handlePostMessage(req, res);
-    } else {
+    }
+    else {
         res.status(400).send('No transport found for sessionId');
     }
 });
-
-server.tool("random-joke", "A joke returned by the chuck norris api", {},
-  async () => {
+server.tool("random-joke", "A joke returned by the chuck norris api", {}, async () => {
     const response = await fetch("https://api.chucknorris.io/jokes/random");
     const data = await response.json();
-
     return {
-      content: [
-        {
-          type: "text",
-          text: data.value
-        }
-      ]
+        content: [
+            {
+                type: "text",
+                text: data.value
+            }
+        ]
     };
-  }
-);
-
+});
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
